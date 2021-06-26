0df9784154188260535e36b29a75941d3c2f9587
Ashley590 <74806292+Ashley590@users.noreply.github.com>
2021-06-26 08:19:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+name_of_the_guest_array = [];
+
+function submit()
+{
+    var name_1 = document.getElementById(name_of_the_guest_1).value;
+    var name_2 = document.getElementById(name_of_the_guest_2).value;
+    var name_3 = document.getElementById(name_of_the_guest_3).value;
+    var name_4 = document.getElementById(name_of_the_guest_4).value;
+
+    name_of_the_guest_array.push(name_1);
+    name_of_the_guest_array.push(name_2);
+    name_of_the_guest_array.push(name_3);
+    name_of_the_guest_array.push(name_4);
+
+    console.log(name_of_the_guest_array);
+
+    document.getElementById("display_name").innerHTML = name_of_the_guest_array;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+}
+
+function sorting()
+{
+    name_of_the_guest_array.sort();
+    console.log(name_of_the_guest_array);
+    document.getElementById("display_name").innerHTML = name_of_the_guest_array;
+}
+
+function searching()
+var s= Document.getElementById("s1").value;
+var found=0;
+var j;
+for (j=0; j<names_of_the_guest.length; j++)
+if (s==names_of_the_guest [j]){
+    found=found+1;
+}
+Document.getElementById("p2").innerHTML="name found"+found+"time/s";
+console.log("found name"+found+"time/s");
+
+
+    
+
